45267da0ee039f8ae30da1d1ec5ad29d1df7630a
cottons-kr <taeyeong.kim@algorix.io>
2024-10-30 23:23:39 +0900

    💄 Add meta tags and favicon to RootLayout for improved SEO and mobile support

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,14 @@ export default function RootLayout(props: ILayoutProps) {
   return <>
     <html lang='ko'>
       <head>
+        <title>스냅플레이스</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' />
+        <meta name='description' content='스냅플레이스로 자신의 추억을 맘껏 남겨보세요!' />
+        <meta name='keywords' content='스냅플레이스, 스냅, 플레이스, snapplace, snap, place' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
+        <link rel='icon' href='/favicon.ico' />
         <script src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_API_JAVASCRIPT_KEY}&libraries=services,clusterer,drawing`} />
       </head>
       <body>
