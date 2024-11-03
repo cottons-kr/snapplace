4fba7f6f99b3ecfb7e67629575dc7c07461ba2b9
cottons-kr <taeyeong.kim@algorix.io>
2024-11-03 23:26:57 +0900

    🎨 Enhance Adjustment Page; add AdjustmentSubmit component and integrate Viewport for layout

--- app/(adjustment)/adjustment/[id]/page.tsx
+++ app/(adjustment)/adjustment/[id]/page.tsx
@@ -5,6 +5,10 @@ import AdjustmentPreview from '@/components/page/adjustment/Preview'
 import { prisma } from '@/lib/prisma'
 import { IParams } from '@/types/props'
 import { redirect } from 'next/navigation'
+import Viewport from '@/components/layout/Viewport'
+import AdjustmentSubmit from '@/components/page/adjustment/Submit'
+
+import s from './style.module.scss'
 
 export default async function AdjustmentPage(props: IParams<'id'>) {
   const params = await props.params
@@ -20,9 +24,12 @@ export default async function AdjustmentPage(props: IParams<'id'>) {
 
   return <>
     <AdjustmentContextWrapper assets={history.images}>
-      <VStack gap={36}>
+      <VStack className={s.page} gap={36} height='100dvh'>
         <AdjustmentPreview />
-        <AdjustmentControl />
+        <Viewport direction='column' height='100%'>
+          <AdjustmentControl />
+        </Viewport>
+        <AdjustmentSubmit />
       </VStack>
     </AdjustmentContextWrapper>
   </>
---
+++ components/page/adjustment/Submit/index.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Flex from '@/components/layout/Flex'
+
+import s from './style.module.scss'
+
+export default function AdjustmentSubmit() {
+  return <>
+    <Flex className={s.submit}>
+      <button>다음</button>
+    </Flex>
+  </>
+}
