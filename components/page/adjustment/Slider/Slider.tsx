799d51e5c184857ba50a354fadcffccea8f054b1
cottons-kr <taeyeong.kim@algorix.io>
2024-11-06 22:01:18 +0900

    🎨 Refactor AdjustmentControlSlider; replace custom slider implementation with native input range and improve styling

--- components/page/adjustment/Slider/Slider.tsx
+++ components/page/adjustment/Slider/Slider.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { HStack } from '@/components/layout/Flex/Stack'
 import classNames from 'classnames'
-import { useCallback, useEffect, useRef, useState } from 'react'
 
 import s from './style.module.scss'
 
@@ -13,65 +11,19 @@ type AdjustmentControlSliderProps = {
 
   onValueChange: (value: number) => unknown
 }
-export default function AdjustmentControlSlider(props: AdjustmentControlSliderProps) {
-  const [isDragging, setIsDragging] = useState(false)
-  const sliderContainerRef = useRef<HTMLDivElement>(null)
-
-  const updateValue = (clientX: number) => {
-    if (sliderContainerRef.current) {
-      const rect = sliderContainerRef.current.getBoundingClientRect()
-      const x = clientX - rect.left
-      const containerWidth = sliderContainerRef.current.offsetWidth
-      let newValue = Math.round((x / containerWidth) * 100)
-      newValue = Math.min(Math.max(newValue, 0), 100)
-      props.onValueChange(newValue)
-    }
-  }
-
-  const handleMouseMove = useCallback((clientX: number) => {
-    if (isDragging) {
-      updateValue(clientX)
-    }
-  }, [isDragging, updateValue])
-
-  const handleMouseDown = useCallback((clientX: number) => {
-    setIsDragging(true)
-    updateValue(clientX)
-  }, [updateValue])
-
-  const handleMouseUp = useCallback(() => {
-    setIsDragging(false)
-  }, [])
-
-  useEffect(() => {
-    const mouseHandler = (e: MouseEvent) => handleMouseMove(e.clientX)
-    const touchHandler = (e: TouchEvent) => handleMouseMove(e.touches[0].clientX)
-
-    document.addEventListener('mousemove', mouseHandler)
-    document.addEventListener('touchmove', touchHandler)
-    document.addEventListener('mouseup', handleMouseUp)
-    document.addEventListener('touchend', handleMouseUp)
-    return () => {
-      document.removeEventListener('mousemove', mouseHandler)
-      document.removeEventListener('touchmove', touchHandler)
-      document.removeEventListener('mouseup', handleMouseUp)
-      document.removeEventListener('touchend', handleMouseUp)
-    }
-  }, [handleMouseMove, handleMouseUp])
-  
+export default function AdjustmentControlSlider(props: AdjustmentControlSliderProps) {  
   return <>
     <div
-      ref={sliderContainerRef} className={s.container}
-      onMouseDown={e => handleMouseDown(e.clientX)}
-      onTouchStart={e => handleMouseDown(e.touches[0].clientX)}
+      className={s.container}
     >
       <p className={s.label}>{props.label}</p>
-      <HStack className={classNames(s.bar, props.sliderClassName)} width='calc(100% - 24px)' height='5px'>
-        <span
-          className={s.dot}
-          style={{ left: `calc(${props.value}% - 6.5px)` }}
-        />
-      </HStack>
+      <input
+        type='range'
+        className={classNames(s.slider, props.sliderClassName)}
+        min={0} max={100}
+        value={props.value}
+        onChange={e => props.onValueChange(parseInt(e.target.value))}
+      />
     </div>
   </>
 }
